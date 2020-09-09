70d977f5496970c9b70be3776c31b5e30c90c645
bodhicougar <info@blackboxmd.fyi>
2020-09-09 16:59:08 +0200

    Navbar angepasst

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -61,16 +61,16 @@ const Navbar = class extends React.Component {
           >
             <div className="navbar-start has-text-centered">
               <Link className="navbar-item" to="/about">
-                About
+                Über
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
+                Services
               </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
               <Link className="navbar-item" to="/contact">
-                Contact
+                Kontakt
               </Link>
               <Link className="navbar-item" to="/contact/examples">
                 Form Examples
